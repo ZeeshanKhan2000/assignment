1a831567dc8d0e443715cbf1f9cb60344d76788d
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2022-11-23 16:16:02 -0800

    Add files via upload

---
+++ couresra assingmnet/js/custom.js
@@ -0,0 +1,92 @@
+// $(".banner-div").owlCarousel({
+//   loop: true,
+//   margin: 0,
+//   nav: true,
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     600: {
+//       items: 1,
+//     },
+//     1000: {
+//       items: 1,
+//     },
+//   },
+// });
+
+// $(".product-slider").owlCarousel({
+//   loop: true,
+//   margin: 60,
+//   nav: true,
+//   responsive: {
+//     0: {
+//       items: 4,
+//     },
+//     600: {
+//       items: 4,
+//     },
+//     1000: {
+//       items: 4,
+//     },
+//   },
+// });
+
+// $(".our-menu-slider ").owlCarousel({
+//   loop: true,
+//   margin: 60,
+//   nav: true,
+//   responsive: {
+//     0: {
+//       items: 4,
+//     },
+//     600: {
+//       items: 4,
+//     },
+//     1000: {
+//       items: 4,
+//     },
+//   },
+// });
+
+
+// *****************************product detail ****************
+
+$('.product-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+
+$('.what-people-say-slider ').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+
